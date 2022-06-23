3466c8af34b34c35c0a1d55d4849809f537e96e6
marikomands <marikomands@gmail.com>
2022-06-23 13:42:40 +0100

    added contextType

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,7 +2,10 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);
     const text = this.context === "English" ? "Name" : "Naam";
     return (
       <div className="ui field">
