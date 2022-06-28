f38efb18d6262425997cae256e6c277b6d6829f0
marikomands <marikomands@gmail.com>
2022-06-28 12:05:27 +0100

    Added Provider to update the state

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+
 class App extends React.Component {
   state = { language: "English" };
 
@@ -25,7 +27,9 @@ class App extends React.Component {
             }}
           />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
