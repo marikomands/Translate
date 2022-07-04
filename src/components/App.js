938f4ed5fc139bf628f39746404ff026e9bb06ea
marikomands <marikomands@gmail.com>
2022-07-04 13:23:15 +0100

    removed view logic to LanguageSelector component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
   state = { language: "English" };
@@ -13,21 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a Language
-          <i
-            className="flag us"
-            onClick={() => {
-              this.onLanguageChange("English");
-            }}
-          />
-          <i
-            className="flag nl"
-            onClick={() => {
-              this.onLanguageChange("Dutch");
-            }}
-          />
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
         <ColorContext.Provider value="red">
           <LanguageContext.Provider value={this.state.language}>
             <UserCreate />
